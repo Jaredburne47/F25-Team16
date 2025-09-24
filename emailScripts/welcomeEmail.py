c79f60b278b6dfb46a7a97d1940246e33c9604e5
isaacberes <156918876+isaacberes@users.noreply.github.com>
2025-09-24 15:09:43 -0400

    Update welcomeEmail.py to contain log in info to new users

--- emailScripts/welcomeEmail.py
+++ emailScripts/welcomeEmail.py
@@ -3,28 +3,35 @@ import ssl
 from email.message import EmailMessage
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python welcomeEmail.py recipient@example.com")
+if len(sys.argv) < 4:
+    print("Usage: python welcomeEmail.py recipient@example.com username password")
     sys.exit(1)
 
 recipient = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
 
 # --- Email account you send from ---
 SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465             
+SMTP_PORT = 465
 SENDER_EMAIL = "team16gdip@gmail.com"
-SENDER_PASSWORD = "jsxl pnun vdts mxhs"
+SENDER_PASSWORD = "jsxl pnun vdts mxhs"  # your app password
 
 # --- Build the email ---
 msg = EmailMessage()
 msg["Subject"] = "Welcome to the Good Driver Incentive Program"
 msg["From"] = SENDER_EMAIL
 msg["To"] = recipient
-msg.set_content("""\
+
+msg.set_content(f"""\
 Hello,
 
 Welcome to the Good Driver Incentive Program! 
 We’re excited to have you on board.
+
+Here are your credentials:
+Username: {username}
+Password: {password}
 
 Team 16
 """)
